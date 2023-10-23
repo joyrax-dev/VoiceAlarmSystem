c2bd2c069b042c7a9ee9bd2fe41cc4dd7d16611b
John Doe <joyrax.dev@gmail.com>
2023-10-23 14:14:34 +1000

    more fix

--- Scripts/Install.bat
+++ Scripts/Install.bat
@@ -5,15 +5,11 @@ setlocal
 echo "Installing"
 ping -n 3 127.0.0.1 >nul:
 
-IF EXIST "C:/VoiceAlarmSystem/node_modules/speaker" (
-    
-) ELSE (
-    :: распаковка модуля спикера
-	powershell -Command "Expand-Archive -Path 'C:/VoiceAlarmSystem/Soft/speaker.zip' -DestinationPath 'C:/VoiceAlarmSystem/node_modules'"
-)
+:: распаковка модуля спикера
+powershell -Command "Expand-Archive -Path 'C:/VoiceAlarmSystem/Soft/speaker.zip' -DestinationPath 'C:/VoiceAlarmSystem/node_modules'"
 
 :: установка сокса
 powershell -Command "Start-Process -FilePath 'C:/VoiceAlarmSystem/Soft/sox-14.4.1-win32.exe' -ArgumentList '/S' -Wait"
 
 :: добавление пути сокса в PATH
-powershell -Command "[Environment]::SetEnvironmentVariable('PATH', [Environment]::GetEnvironmentVariable('PATH', 'Machine') + 'C:\Program Files (x86)\sox-14.4.1', 'Machine')"
+powershell -Command "[Environment]::SetEnvironmentVariable('PATH', [Environment]::GetEnvironmentVariable('PATH', 'Machine') + ';' + 'C:\Program Files (x86)\sox-14.4.1', 'Machine')"
