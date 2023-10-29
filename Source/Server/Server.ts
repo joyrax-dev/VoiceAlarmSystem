04f709e92f2767f7d106f5d6d15f920341082ca9
John Doe <joyrax.dev@gmail.com>
2023-10-29 12:39:41 +1000

    fix

--- Source/Server/Server.ts
+++ Source/Server/Server.ts
@@ -12,10 +12,6 @@ export default function Start() {
     SocketServer.on('connection', (socket: Socket) => {
         StartHandlers(socket, SocketServer)
 
-        setInterval(() => {
-            socket.emit('get_latency')
-        }, 5000)
-        
         console.log(`Client connected [id=${socket.id}]`)
     })
 
