0c65cead2d9525afa1735a26c58e0943ae3952fb
John Doe <joyrax.dev@gmail.com>
2023-10-31 10:33:18 +1000

    v3 (add installer)

--- Packing.ps1
+++
@@ -1,6 +0,0 @@
-
-$compress = @{
-	LiteralPath= "C:\VoiceAlarmSystem\node_modules", "C:\VoiceAlarmSystem\Build", "C:\VoiceAlarmSystem\package.json", "C:\VoiceAlarmSystem\ecosystem.config.js"
-	DestinationPath = "C:\VoiceAlarmSystem\Package.zip"
-}
-Compress-Archive @compress
