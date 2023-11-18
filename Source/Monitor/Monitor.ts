e214370c0c82806432b2892a48a0e3f6996e83dd
John Doe <joyrax.dev@gmail.com>
2023-11-18 14:06:58 +1000

    moded monitor

--- Source/Monitor/Monitor.ts
+++ Source/Monitor/Monitor.ts
@@ -2,6 +2,7 @@ import blessed from 'blessed'
 import { Socket, connect } from 'socket.io-client'
 import fs from 'fs'
 import { Host, Port } from '../monitor.json'
+import { IClientInfo } from '../Shared/IClientInfo'
 
 class Monitor {
 	private _socket: Socket
@@ -28,7 +29,7 @@ class Monitor {
 			top: '0',
 			left: '0',
 			width: '100%',
-			height: '60%',
+			height: '65%',
 			align: 'center',
 			tags: true,
 			border: {
@@ -48,10 +49,10 @@ class Monitor {
 
 	private CreateLog() {
 		return blessed.log({
-			top: '60%',
+			top: '65%',
 			left: '0',
 			width: '100%',
-			height: '40%',
+			height: '35%',
 			scrollable: true,
 			tags: true,
 			border: {
@@ -72,24 +73,24 @@ class Monitor {
 		})
 	}
 
-	public Loop() {
-		this.Table = this.CreateTable()
-		this.Log = this.CreateLog()
-
-		this.Screen.append(this.Table)
-		this.Screen.append(this.Log)
-
-		this.Update()
-		this.Screen.render()
+	public Start() {
+		setInterval(() => {
+			this.UpdateStatus()
+			this.UpadateLogs()
+			this.Screen.render()
+		}, 2000)
 	}
 
-	public Update() {
+	public UpadateLogs() {
+		this.Log = this.CreateLog()
+		this.Screen.append(this.Log)
+
 		function load_log(): string[] {
 			const file_path = 'Logs/vas.log'
 
 			const raw = fs.readFileSync(file_path, 'utf8')
 			const lines = raw.split('\n')
-			const last10Lines = lines.slice(-10)
+			const last10Lines = lines.slice(-7)
 
 			return last10Lines
 		}
@@ -99,19 +100,67 @@ class Monitor {
 		for(const log of lastLogs) {
 			this.Log.log(log)
 		}
-
-		this.Table.setData(this.TableData)
-
-		this._socket.volatile.emit('get_stats', (data) => {
-			this.TableData = [ ['id', 'location', 'type'] ]
-			
-			for(const stat in data) {
-				const temp_arr = []
-				temp_arr.push(stat, data[stat].Location, data[stat].Type)
-				this.TableData.push(temp_arr)
-			}
-		})
 	}
+
+	public UpdateStatus() { //AuthorizedUsers.json
+		this.Table = this.CreateTable()
+		this.Screen.append(this.Table)
+
+		const data = [ ['id', 'location', 'type'] ]
+
+		function load_auth(): Record<string, IClientInfo> {
+			const file_path = 'AuthorizedUsers.json'
+
+			const raw = fs.readFileSync(file_path, 'utf8')
+
+			return JSON.parse(raw)
+		}
+
+		const auths = load_auth()
+
+		const sortedAuths = Object.entries(auths).sort((a: [string, IClientInfo], b: [string, IClientInfo]) => {
+			// Сортировка по Location
+			if (a[1].Location < b[1].Location) {
+				return -1
+			}
+			if (a[1].Location > b[1].Location) {
+				return 1
+			}
+
+			// Если Location одинаковый, сортировка по Type
+			if (a[1].Type < b[1].Type) {
+				return 1
+			}
+			if (a[1].Type > b[1].Type) {
+				return -1
+			}
+
+			return 0
+		})
+
+		sortedAuths.forEach(([key, value]) => {
+			const temp_arr = []
+			temp_arr.push(key, value.Location, value.Type)
+			data.push(temp_arr)
+		})
+
+		this.Table.setData(data)
+	}
+
+	// public Update() {
+		
+		
+
+	// 	this._socket.volatile.emit('get_stats', (data) => {
+	// 		this.TableData = [ ['id', 'location', 'type'] ]
+			
+	// 		for(const stat in data) {
+	// 			const temp_arr = []
+	// 			temp_arr.push(stat, data[stat].Location, data[stat].Type)
+	// 			this.TableData.push(temp_arr)
+	// 		}
+	// 	})
+	// }
 }
 
 export default function Start() {
@@ -126,7 +175,5 @@ export default function Start() {
 	
 	socket.connect()
 
-	setInterval(() => {
-		monitor.Loop()
-	}, 2000)
+	monitor.Start()
 }
